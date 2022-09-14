d6bca58d3c3955fd978385380b65931dc72e9fd4
imation82 <odesk.rahman@gmail.com>
2022-09-14 23:20:57 +0600

    Completed Introduction Section

--- components/Introduction.vue
+++ components/Introduction.vue
@@ -1,16 +1,47 @@
 <template>
     <section class="introduction">
         <div class="container">
-            <div class="row">
-                <div class="col-lg-7">
-                    <div class="introduction__left">
+            <div class="row d-flex align-items-center">
+                <div class="col-lg-8">
+                    <div class="introduction__left align-items-center">
                         <h2>Video Time Capsule</h2>
                         <h1>Secure Life Stories and Family History for Future Generations</h1>
                         <p class="mt-3">Video Time Capsule helps you to secure and share your family's history.</p>
+                        <div class="introduction__left--apps">
+                            <ul class="list-inline">
+                                <li class="list-inline-item introduction__left--apps__ios">
+                                    <a href="#">
+                                        <img src="~assets/svg/ios.svg" alt="iOS">
+                                    </a>
+                                </li>
+                                <li class="list-inline-item">
+                                    <a href="#">
+                                        <img src="~assets/svg/google-play.svg" alt="Google Play Store">
+                                    </a>                                    
+                                </li>
+                            </ul>
+                        </div>
                     </div>                    
                 </div>
-                <div class="col-lg-5">
-                    
+                <div class="col-lg-4">
+                    <div class="introduction__right">
+                        <div class="row">
+                            <div class="col-lg-6">
+                                <img src="~assets/svg/intro-1.svg" alt="Intro 1" class="float-end mb-2">
+                            </div>
+                            <div class="col-lg-6">
+                                <img src="~assets/svg/intro-2.svg" alt="Intro 2" class="mb-2">
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-lg-6">
+                                <img src="~assets/svg/intro-3.svg" alt="Intro 3" class="float-end">
+                            </div>
+                            <div class="col-lg-6">
+                                <img src="~assets/svg/intro-4.svg" alt="Intro 4">
+                            </div>
+                        </div>                        
+                    </div>
                 </div>
             </div>
         </div>
