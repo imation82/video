6f5f0bb027cf841548457fb61935039b38990701
imation82 <odesk.rahman@gmail.com>
2022-09-15 00:22:24 +0600

    Completed History section

--- components/History.vue
+++ components/History.vue
@@ -6,6 +6,24 @@
                     <div class="history__headline d-block mx-auto text-center">
                         <h2>Video Time Capsule is the<br>easy way to <span>SECURE</span>, <span>STORE</span> and <span>SHARE</span> videos about your life history</h2>
                     </div>
+                    <div class="history__capsule">
+                        <div class="row">
+                            <div class="col-lg-10 offset-lg-1 d-flex">
+                                <div class="col-lg-6">
+                                    <img src="~assets/svg/easy-to-use.svg" alt="Easy to use" class="d-block mx-auto">
+                                    <div class="history__capsule--content mt-3 text-center">
+                                        <h3>Easy to Use</h3>
+                                    </div>
+                                </div>
+                                <div class="col-lg-6">
+                                    <img src="~assets/svg/easy-to-share.svg" alt="Easy to share" class="d-block mx-auto">
+                                    <div class="history__capsule--content mt-3 text-center">
+                                        <h3>Easy to Share</h3>
+                                    </div>                                    
+                                </div>
+                            </div>
+                        </div>                        
+                    </div>
                 </div>
             </div>
         </div>
