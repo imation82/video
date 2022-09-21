92bc899fe68d8867eef9b45ab713f8f14f862e31
Ashfaqur Rahman <odesk.rahman@gmail.com>
2022-09-21 18:54:34 +0600

    Update

--- components/Testimonial.vue
+++ components/Testimonial.vue
@@ -8,6 +8,47 @@
                     </div>
                 </div>
             </div>
+            <div class="row">
+                <div class="col-lg-4">
+                    <div class="testimonial__slider text-center">
+                        <p>Disable wireframe mode at any time to render your project as a high-fidelity mockup.</p>
+                        <div class="testimonial__content d-flex justify-content-center align-items-center">
+                            <div>
+                                <img src="~assets/images/testimonial/client-1.jpg" alt="Client">
+                            </div>
+                            <div>
+                                <h4>Daanvielen</h4>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4">
+                    <div class="testimonial__slider text-center">
+                        <p>Disable wireframe mode at any time to render your project as a high-fidelity mockup.</p>
+                        <div class="testimonial__content d-flex justify-content-center align-items-center">
+                            <div>
+                                <img src="~assets/images/testimonial/client-2.jpg" alt="Client">
+                            </div>
+                            <div>
+                                <h4>Thewiscokid</h4>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4">
+                    <div class="testimonial__slider text-center">
+                        <p>Disable wireframe mode at any time to render your project as a high-fidelity mockup.</p>
+                        <div class="testimonial__content d-flex justify-content-center align-items-center">
+                            <div>
+                                <img src="~assets/images/testimonial/client-1.jpg" alt="Client">
+                            </div>
+                            <div>
+                                <h4>Daanvielen</h4>
+                            </div>
+                        </div>
+                    </div>
+                </div>                                
+            </div>
         </div>
     </section>
 </template>
