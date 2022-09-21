83146dc4f3e3aa41e434e43d9c799346b08fc2f3
Ashfaqur Rahman <odesk.rahman@gmail.com>
2022-09-21 15:59:37 +0600

    Members

---
+++ components/Team.vue
@@ -0,0 +1,115 @@
+<template>
+    <section class="team">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="team__headline d-block mx-auto text-center">
+                        <h3>Meet our team</h3>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-3">
+                    <div class="team__profile mb-4">
+                        <img src="~assets/images/team/nicholas.jpg" alt="Nicholas" class="img-fluid d-block mx-auto w-100">
+                        <div class="team__profile--content text-center">
+                            <h4>Nicholas P</h4>
+                            <h5>Founder CEO</h5>
+                            <span>Burlington, Iowa</span>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3">
+                    <div class="team__profile mb-4">
+                        <img src="~assets/images/team/samantha.jpg" alt="Samantha" class="img-fluid d-block mx-auto w-100">
+                        <div class="team__profile--content text-center">
+                            <h4>Samantha P</h4>
+                            <h5>Founder</h5>
+                            <span>Iowa City, Iowa</span>
+                        </div>
+                    </div>                    
+                </div>
+                <div class="col-lg-3">
+                    <div class="team__profile mb-4">
+                        <img src="~assets/images/team/nicole.jpg" alt="Nicole" class="img-fluid d-block mx-auto w-100">
+                        <div class="team__profile--content text-center">
+                            <h4>Nicole P</h4>
+                            <h5>Co-Founder</h5>
+                            <span>Pine Mountain, Georgia</span>
+                        </div>
+                    </div>                     
+                </div>
+                <div class="col-lg-3">
+                    <div class="team__profile mb-4">
+                        <img src="~assets/images/team/mitchell.jpg" alt="Mitchell" class="img-fluid d-block mx-auto w-100">
+                        <div class="team__profile--content text-center">
+                            <h4>Mitchell N</h4>
+                            <h5>Co-Founder</h5>
+                            <span>Fort Madison, Iowa</span>
+                        </div>
+                    </div>                    
+                </div>
+                <div class="col-lg-3">
+                    <div class="team__profile mb-4">
+                        <img src="~assets/images/team/dane.jpg" alt="Dane" class="img-fluid d-block mx-auto w-100">
+                        <div class="team__profile--content text-center">
+                            <h4>Dane B</h4>
+                            <h5>VP Project Manager</h5>
+                            <span>Vernon, Washington</span>
+                        </div>
+                    </div>                    
+                </div> 
+                <div class="col-lg-3">
+                    <div class="team__profile mb-4">
+                        <img src="~assets/images/team/hasan.jpg" alt="Hasan" class="img-fluid d-block mx-auto w-100">
+                        <div class="team__profile--content text-center">
+                            <h4>Hasan H</h4>
+                            <h5>Advisor</h5>
+                            <span>Manhattan, New York</span>
+                        </div>
+                    </div>                    
+                </div> 
+                <div class="col-lg-3">
+                    <div class="team__profile mb-4">
+                        <img src="~assets/images/team/julia.jpg" alt="Julia" class="img-fluid d-block mx-auto w-100">
+                        <div class="team__profile--content text-center">
+                            <h4>Julia M</h4>
+                            <h5>Consultant</h5>
+                            <span>Spain</span>
+                        </div>
+                    </div>                    
+                </div> 
+                <div class="col-lg-3">
+                    <div class="team__profile mb-4">
+                        <img src="~assets/images/team/ariful.jpg" alt="Ariful" class="img-fluid d-block mx-auto w-100">
+                        <div class="team__profile--content text-center">
+                            <h4>Ariful H</h4>
+                            <h5>UI/UX Designer</h5>
+                            <span>Bangladesh</span>
+                        </div>
+                    </div>                    
+                </div>
+                <div class="col-lg-3">
+                    <div class="team__profile mb-4">
+                        <img src="~assets/images/team/rahima.jpg" alt="Rahima" class="img-fluid d-block mx-auto w-100">
+                        <div class="team__profile--content text-center">
+                            <h4>Rahima S</h4>
+                            <h5>Programmer</h5>
+                            <span>Bangladesh</span>
+                        </div>
+                    </div>                    
+                </div>
+                <div class="col-lg-3">
+                    <div class="team__profile mb-4">
+                        <img src="~assets/images/team/ward.jpg" alt="Ward" class="img-fluid d-block mx-auto w-100">
+                        <div class="team__profile--content text-center">
+                            <h4>Ward G</h4>
+                            <h5>Programmer</h5>
+                            <span>Amman, Jordan</span>
+                        </div>
+                    </div>                    
+                </div>                                                                                                
+            </div>
+        </div>
+    </section>
+</template>
