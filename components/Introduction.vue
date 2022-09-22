2ec6fa9333588e7b2ed10428485102cd0f3947fa
imation82 <odesk.rahman@gmail.com>
2022-09-22 22:33:33 +0600

    Update

--- components/Introduction.vue
+++ components/Introduction.vue
@@ -7,7 +7,7 @@
                         <h2>Video Time Capsule</h2>
                         <h1>Secure Life Stories and Family History for Future Generations</h1>
                         <p class="mt-3">Video Time Capsule helps you to secure and share your family's history.</p>
-                        <div class="introduction__left--apps">
+                        <div class="introduction__left--apps d-none d-lg-block">
                             <ul class="list-inline text-center text-md-start">
                                 <li class="list-inline-item introduction__left--apps__ios">
                                     <a href="https://apps.apple.com/us/app/videotc/id1554817576" target="_blank">
@@ -26,22 +26,36 @@
                 <div class="col-lg-4">
                     <div class="introduction__right">
                         <div class="row">
-                            <div class="col-lg-6 col-6">
-                                <img src="~assets/svg/intro-1.svg" alt="Intro 1" class="img-fluid float-lg-end mb-4">
+                            <div class="col-lg-6 col-md-6 col-6">
+                                <img src="~assets/svg/intro-1.svg" alt="Intro 1" class="d-block mx-auto float-end img-fluid float-lg-end mb-4">
                             </div>
-                            <div class="col-lg-6 col-6">
-                                <img src="~assets/svg/intro-2.svg" alt="Intro 2" class="img-fluid mb-4">
+                            <div class="col-lg-6 col-md-6 col-6">
+                                <img src="~assets/svg/intro-2.svg" alt="Intro 2" class="d-block mx-auto float-start img-fluid mb-4">
                             </div>
                         </div>
                         <div class="row">
-                            <div class="col-lg-6 col-6">
-                                <img src="~assets/svg/intro-3.svg" alt="Intro 3" class="img-fluid float-lg-end">
+                            <div class="col-lg-6 col-md-6 col-6">
+                                <img src="~assets/svg/intro-3.svg" alt="Intro 3" class="d-block mx-auto img-fluid float-end float-lg-end">
                             </div>
-                            <div class="col-lg-6 col-6">
-                                <img src="~assets/svg/intro-4.svg" alt="Intro 4" class="img-fluid">
+                            <div class="col-lg-6 col-md-6 col-6">
+                                <img src="~assets/svg/intro-4.svg" alt="Intro 4" class="d-block mx-auto float-start img-fluid">
                             </div>
                         </div>                        
                     </div>
+                    <div class="introduction__left--apps d-block d-lg-none">
+                        <ul class="list-inline text-lg-start mb-0 d-flex justify-content-center">
+                            <li class="list-inline-item introduction__left--apps__ios">
+                                <a href="https://apps.apple.com/us/app/videotc/id1554817576" target="_blank">
+                                    <img src="~assets/svg/ios.svg" class="img-fluid" alt="iOS">
+                                </a>
+                            </li>
+                            <li class="list-inline-item">
+                                <a href="https://play.google.com/store/apps/details?id=com.videotc.vtc_mobile_app_v2" target="_blank">
+                                    <img src="~assets/svg/google-play.svg" class="img-fluid" alt="Google Play Store">
+                                </a>                                    
+                            </li>
+                        </ul>
+                    </div>                    
                 </div>
             </div>
         </div>
