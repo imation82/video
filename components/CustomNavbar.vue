50a42600217d9785935ab008f556ac7211bf36a0
Ashfaqur Rahman <odesk.rahman@gmail.com>
2022-10-10 11:51:41 +0600

    Site update

--- components/CustomNavbar.vue
+++ components/CustomNavbar.vue
@@ -2,13 +2,13 @@
     <section class="customnav">
         <div class="container">
             <div class="row">
-                <div class="col-lg-6">
+                <div class="col-lg-6 col-6">
                     <a href="/">
                         <img src="~assets/images/logo.png" alt="Logo">
                     </a>
                 </div>
-                <div class="col-lg-6">
-
+                <div class="col-lg-6 col-6 d-flex align-items-center justify-content-end">
+                    <a href="/" class="btn btn-success">⇠&nbsp;&nbsp;Back to home</a>
                 </div>
             </div>
         </div>
