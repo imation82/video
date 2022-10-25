dba12911fedc07cfa80315a076d14f02d0870b23
Ashfaqur Rahman <odesk.rahman@gmail.com>
2022-10-25 15:15:46 +0600

    404 added

---
+++ layouts/error.vue
@@ -0,0 +1,25 @@
+<template>
+    <section class="error vh-100 d-flex align-items-center justify-content-center flex-column">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <img src="~assets/images/404.gif" alt="Error" class="mx-auto d-block img-fluid">
+                        <div class="text-center">
+                            <h2>Ooops! Seems you lost in the river</h2>
+                            <div class="mt-3">
+                                <a href="/" class="btn-back">Back To Home</a>                                
+                            </div>
+                        </div>                        
+                </div>
+            </div>
+        </div>        
+    </section>
+</template>
+
+<script>
+
+export default {
+    name: "Error"
+}
+
+</script>
