7db525384587f96373f5499cf2c2792cbc983698
Ashfaqur Rahman <odesk.rahman@gmail.com>
2022-10-25 15:35:22 +0600

    Update fallback

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,6 +5,11 @@ export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: "static",
 
+  // 404 fallback
+  generate: {
+    fallback: true
+  },  
+
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: 'VideoTC',
